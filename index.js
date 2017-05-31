74bbba9ee0c3b797aa91c0fd6914151625083982
gaopeng <gaopeng@lekee.cc>
2017-05-31 22:53:14 +0800

    完成 循环 一节

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var app = new Vue({
   el: '#app',
-  methods: {
-    logMessage() {
-      console.log(this.message)
-    }
-  },
   data: {
-    message: 'hold',
-    title: 'hello',
-    welcome: true
+    comments: [
+      { content: 'nice ~'},
+      { content: 'great ~'},
+      { content: 'swesome ~'},
+    ]
   }
 })
