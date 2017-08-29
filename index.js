a7ff7cb1d22615d7145bcb0ca7233ad1c53324e5
gaopeng <service@lekee.cc>
2017-08-29 21:00:32 +0800

    finsh router

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const app = new Vue({
-  el: '#app'
+const Home = {
+  template: '<h2>首页</h2>'
+}
+
+const Event = {
+  template: '<h2>活动</h2>'
+}
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/events',
+    component: Event
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+  const app = new Vue({
+  el: '#app',
+  router
 })
